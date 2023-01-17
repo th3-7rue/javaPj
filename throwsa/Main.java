135b245588890ee96957c53c7929345be38fae26
th3-7rue <66943187+th3-7rue@users.noreply.github.com>
2023-01-17 10:32:17 +0000

    h

--- throwsa/Main.java
+++ throwsa/Main.java
@@ -4,28 +4,31 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws ArithmeticException {
-        Scanner tastiera = new Scanner(System.in);
-        System.out.print("Inserisci l'ora: ");
-        int h = Integer.parseInt(tastiera.nextLine());
-        if (h >= 24 || h < 0) {
-            throw new ArithmeticException("Orario sbagliato");
-        } else {
-            System.out.println("Orario corretto");
-            System.out.print("Inserisci i minuti: ");
-            int m = Integer.parseInt(tastiera.nextLine());
-            if (m>=60||m<0) {
+        try (Scanner tastiera = new Scanner(System.in)) {
+            System.out.print("Inserisci l'ora: ");
+            int h = Integer.parseInt(tastiera.nextLine());
+            if (h >= 24 || h < 0) {
                 throw new ArithmeticException("Orario sbagliato");
             } else {
                 System.out.println("Orario corretto");
-                System.out.print("Inserisci i secondi: ");
-                int s = Integer.parseInt(tastiera.nextLine());
-                if (s >= 60 || s < 0) {
+                System.out.print("Inserisci i minuti: ");
+                int m = Integer.parseInt(tastiera.nextLine());
+                if (m>=60||m<0) {
                     throw new ArithmeticException("Orario sbagliato");
                 } else {
                     System.out.println("Orario corretto");
+                    System.out.print("Inserisci i secondi: ");
+                    int s = Integer.parseInt(tastiera.nextLine());
+                    if (s >= 60 || s < 0) {
+                        throw new ArithmeticException("Orario sbagliato");
+                    } else {
+                        System.out.println("Orario corretto");
 
+                    }
                 }
             }
+        } catch (NumberFormatException e) {
+            e.printStackTrace();
         }
     }
 }
