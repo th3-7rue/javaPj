bdcb8b5cf82ab16a9f7a478b9b6f64716a64f18c
th3-7rue <66943187+th3-7rue@users.noreply.github.com>
2023-01-17 12:48:04 +0000

    d

--- es30/Porto.java
+++ es30/Porto.java
@@ -1,9 +1,33 @@
 package es30;
 
+import java.util.Scanner;
+
 public class Porto {
     private Barca[] b;
 
     public Porto() {
         b = new Barca[100];
+    }
+
+    private void assegnaPosto(int posto) {
+        Scanner tastiera = new Scanner(System.in);
+        System.out.print("Inserisci nome: ");
+        String nome = tastiera.nextLine();
+        System.out.print("Inserisci nazionalita': ");
+        String nazionalita = tastiera.nextLine();
+        System.out.print("Inserisci lunghezza: ");
+        float lunghezza = Float.parseFloat(tastiera.nextLine());
+        System.out.print("Inserisci stazza: ");
+        float stazza = Float.parseFloat(tastiera.nextLine());
+        System.out.print("Inserisci tipologia: ");
+        String tipologia = tastiera.nextLine();
+        try {
+            b[posto] = new Barca(nome, nazionalita, tipologia, lunghezza, stazza);
+            if
+        } catch (ArrayStoreException e) {
+            System.out.println("Errore: uno o piu' dati non validi");
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("Errore: osizione non valida");
+        }
     }
 }
