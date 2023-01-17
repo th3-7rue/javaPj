35522d871e3bddf823c7caa00875905c83d5f6e3
th3-7rue <66943187+th3-7rue@users.noreply.github.com>
2023-01-17 10:30:46 +0000

    a

---
+++ throwsa/Main.java
@@ -0,0 +1,31 @@
+package throwsa;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws ArithmeticException {
+        Scanner tastiera = new Scanner(System.in);
+        System.out.print("Inserisci l'ora: ");
+        int h = Integer.parseInt(tastiera.nextLine());
+        if (h >= 24 || h < 0) {
+            throw new ArithmeticException("Orario sbagliato");
+        } else {
+            System.out.println("Orario corretto");
+            System.out.print("Inserisci i minuti: ");
+            int m = Integer.parseInt(tastiera.nextLine());
+            if (m>=60||m<0) {
+                throw new ArithmeticException("Orario sbagliato");
+            } else {
+                System.out.println("Orario corretto");
+                System.out.print("Inserisci i secondi: ");
+                int s = Integer.parseInt(tastiera.nextLine());
+                if (s >= 60 || s < 0) {
+                    throw new ArithmeticException("Orario sbagliato");
+                } else {
+                    System.out.println("Orario corretto");
+
+                }
+            }
+        }
+    }
+}
