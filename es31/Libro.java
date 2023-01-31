a9022f10673e9c5e317365718fd2ac50727e09eb
th3-7rue <66943187+th3-7rue@users.noreply.github.com>
2023-01-31 10:36:58 +0000

    h

--- es31/Libro.java
+++ es31/Libro.java
@@ -1,3 +1,22 @@
 public class Libro {
-    
+    private String autore, titolo, editore;
+    private int anno;
+    public Libro(String autore, String titolo, String editore, int anno) {
+        this.autore = autore;
+        this.titolo = titolo;
+        this.editore = editore;
+        this.anno = anno;
+    }
+    public String getAutore() {
+        return autore;
+    }
+    public String getTitolo() {
+        return titolo;
+    }
+    public String getEditore() {
+        return editore;
+    }
+    public int getAnno() {
+        return anno;
+    }
 }
