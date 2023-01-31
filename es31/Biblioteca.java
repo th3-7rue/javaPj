7eb046f06c6e0f3e8f0195c1afd045441ee224f8
th3-7rue <66943187+th3-7rue@users.noreply.github.com>
2023-01-31 11:50:40 +0000

    fd

--- es31/Biblioteca.java
+++ es31/Biblioteca.java
@@ -17,13 +17,13 @@ public class Biblioteca {
             Scanner t = new Scanner(System.in);
             System.out.println("Aggiunta di un libro:");
             System.out.print("Inserisci l'autore: ");
-            autore = t.next();
+            autore = t.nextLine();
             System.out.print("Inserisci il titolo: ");
-            titolo = t.next();
+            titolo = t.nextLine();
             System.out.print("Inserisci l'anno di pubblicazione: ");
             anno = t.nextInt();
             System.out.print("Inserisci l'editore: ");
-            editore = t.next();
+            editore = t.nextLine();
             System.out.print("Inserisci la posizione: ");
             posizione = t.nextInt();
             if (l[posizione] != null) {
@@ -31,7 +31,7 @@ public class Biblioteca {
             }
             l[posizione] = new Libro(autore, titolo, editore, anno);
         } catch (InputMismatchException e) {
-            System.out.println("Errore: dati non validi");
+            System.out.println("Errore: dato non valido");
         } catch (ArrayStoreException e) {
             System.out.println("Errore: dati non validi");
         } catch (ArrayIndexOutOfBoundsException | IllegalArgumentException e) {
