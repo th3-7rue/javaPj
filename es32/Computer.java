b24a54d791620e8e7573efd151da5c1fcb267a3b
th3-7rue <66943187+th3-7rue@users.noreply.github.com>
2023-02-07 10:33:08 +0000

    g

--- es32/Computer.java
+++ es32/Computer.java
@@ -5,5 +5,17 @@ public class Computer {
     private int anno;
     private float clockCPU, dimRAM, dimDisk, dimMonitor;
     private String marca, modello;
-    
+
+    public Computer(int codice, int anno, float clockCPU, float dimRAM, float dimDisk, float dimMonitor, String marca,
+            String modello) {
+        this.codice = codice;
+        this.anno = anno;
+        this.clockCPU = clockCPU;
+        this.dimRAM = dimRAM;
+        this.dimDisk = dimDisk;
+        this.dimMonitor = dimMonitor;
+        this.marca = marca;
+        this.modello = modello;
+    }
+
 }
