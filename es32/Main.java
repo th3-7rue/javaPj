8a1d72814632b578e7460b99adc09ef59f143541
th3-7rue <66943187+th3-7rue@users.noreply.github.com>
2023-02-07 10:44:43 +0000

    g

--- es32/Main.java
+++ es32/Main.java
@@ -1,6 +1,59 @@
 package es32;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        int scelta = -1;
+        Gestione b = new Gestione();
+        do {
+            try {
+                scelta = -1;
+                Scanner t = new Scanner(System.in);
+                System.out.println("Menu:");
+                System.out.println("0. Esci");
+                System.out.println("1. Aggiungi");
+                System.out.println("2. Elimina");
+                System.out.println("3. Ricerca codice");
+                System.out.println("4. Ricerca caratteristiche");
+                System.out.println("5. Salva");
+                System.out.println("6. Ripristina");
+
+                if (t.hasNext()) {
+                    scelta = t.nextInt();
+                }
+                if (scelta < 0 || scelta > 5) {
+                    throw new IllegalArgumentException();
+                }
+            } catch (InputMismatchException | IllegalArgumentException e) {
+                System.out.println("Errore: scelta non valida");
+            }
+            switch (scelta) {
+                case 0:
+                    System.out.println("Uscita in corso");
+                    break;
+                case 1:
+                    System.out.println(b.aggiungi());
+                    break;
+                case 2:
+                    System.out.println(b.elimina());
+                    break;
+                case 3:
+                    System.out.println(b.ricercaCodice());
+                    break;
+                case 4:
+                    System.out.println(b.ricercaCaratteristiche());
+                    break;
+                case 5:
+                    System.out.println(b.salva());
+                    break;
+                case 6:
+                    System.out.println(b.ripristina());
+                    break;
+                default:
+                    break;
+            }
+        } while (scelta != 0);
     }
 }
