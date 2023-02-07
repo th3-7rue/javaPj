c343b1615fbee61332785294d9683c1b6e4bc5ab
th3-7rue <66943187+th3-7rue@users.noreply.github.com>
2023-02-07 10:26:00 +0000

    f

--- es32/Computer.java
+++ es32/Computer.java
@@ -1,5 +1,9 @@
 package es32;
 
 public class Computer {
+    private int codice;
+    private int anno;
+    private float clockCPU, dimRAM, dimDisk, dimMonitor;
+    private String marca, modello;
     
 }
--- es32/Main.java
+++ es32/Main.java
@@ -2,6 +2,5 @@ package es32;
 
 public class Main {
     public static void main(String[] args) {
-        
     }
 }
