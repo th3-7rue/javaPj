e57b26e75585627bed2c2fb5bd32233092b8415c
th3-7rue <riccardorasori2005@gmail.com>
2023-02-12 17:12:32 +0100

    a

---
+++ liste0/Lista.java
@@ -0,0 +1,11 @@
+package liste0;
+
+public class Lista {
+    private Nodo head;
+    private int elementi;
+
+    public Lista() {
+        head = null;
+        elementi = 0;
+    }
+}
--- liste0/Paziente.java
+++ liste0/Paziente.java
@@ -1,5 +1,15 @@
 package liste0;
 
 public class Paziente {
+    private String nome, urgenza;
 
+    public Paziente(String nome, String urgenza) {
+        this.nome = nome;
+        this.urgenza = urgenza;
+    }
+
+    public Paziente(Paziente paziente) {
+        this.nome = paziente.nome;
+        this.urgenza = paziente.urgenza;
+    }
 }
