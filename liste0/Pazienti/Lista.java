3892ee6963784aa78ca5e43badee1ec48696650d
th3-7rue <riccardorasori2005@gmail.com>
2023-02-17 11:17:44 +0100

    d

--- liste0/Pazienti/Lista.java
+++ liste0/Pazienti/Lista.java
@@ -2,11 +2,9 @@ package Pazienti;
 
 public class Lista {
     private Nodo head;
-    private int elementi;
 
     public Lista() {
         head = null;
-        elementi = 0;
     }
 
     public String aggiungiPaziente(Paziente paziente) {
