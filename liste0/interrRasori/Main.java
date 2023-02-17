79b5d152e5383630ca5b770dc91064839ff68bf6
th3-7rue <riccardorasori2005@gmail.com>
2023-02-17 11:13:08 +0100

    h

--- liste0/interrRasori/Main.java
+++ liste0/interrRasori/Main.java
@@ -1,9 +1,9 @@
 class Main {
     public static void main(String[] args) {
         Lista l = new Lista();
-        l.inserisciInTesta('f');
-        l.inserisciInTesta('o');
-        l.inserisciInTesta('a');
+        // l.inserisciInTesta('f');
+        // l.inserisciInTesta('o');
+        // l.inserisciInTesta('a');
         l.inserisciInTesta('i');
         l.inserisciInTesta('c');
         System.out.println(l);
