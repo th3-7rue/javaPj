3cc2b538320b4679c931419674ce84b77e9be839
th3-7rue <66943187+th3-7rue@users.noreply.github.com>
2023-02-28 16:40:15 +0000

    t

--- liste0/Pazienti/Main.java
+++ liste0/Pazienti/Main.java
@@ -3,10 +3,12 @@ package Pazienti;
 public class Main {
     public static void main(String[] args) {
         Ospedale l = new Ospedale();
-        Paziente mattia = new Paziente("Mattia Baritono", 1);
-        Paziente j = new Paziente("Omar", 3);
+        Paziente mattia = new Paziente("Mattia Baritono", 3);
+        Paziente j = new Paziente("Omar", 1);
+        Paziente k = new Paziente("semo", 3);
         l.aggiungiPaziente(mattia);
         l.aggiungiPaziente(j);
+        l.aggiungiPaziente(k);
         l.stampaLista();
         System.out.println(l.prossimoPaziente());
     }
