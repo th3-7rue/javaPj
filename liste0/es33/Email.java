f2f12cdbc94d9de41ff0d62081288b4eb9f6581c
th3-7rue <riccardorasori2005@gmail.com>
2023-03-01 10:51:27 +0100

    a

--- liste0/es33/Email.java
+++ liste0/es33/Email.java
@@ -4,4 +4,18 @@ public class Email {
     private String mittente, oggetto, testo;
     private String dataEora;
 
+    public Email(String mittente, String oggetto, String testo, String dataEora) {
+        this.mittente = mittente;
+        this.oggetto = oggetto;
+        this.testo = testo;
+        this.dataEora = dataEora;
+    }
+
+    public Email(Email email) {
+        this.mittente = email.mittente;
+        this.oggetto = email.oggetto;
+        this.testo = email.testo;
+        this.dataEora = email.dataEora;
+
+    }
 }
