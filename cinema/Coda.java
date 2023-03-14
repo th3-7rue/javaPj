31dc78381c7b28f74814fa98b06cf3a0d3b1a6f5
th3-7rue <66943187+th3-7rue@users.noreply.github.com>
2023-03-14 11:35:16 +0000

    h

--- cinema/Coda.java
+++ cinema/Coda.java
@@ -1,5 +1,27 @@
 package cinema;
 
+import java.util.Scanner;
+
 public class Coda {
-    
+    private Nodo head;
+    private Nodo tail;
+
+    public Coda() {
+        head = null;
+        tail = null;
+    }
+
+    public String addPersona() {
+        System.out.print("Inserisci nome: ");
+        Scanner t = new Scanner(System.in);
+        String nome = t.nextLine();
+        Persona p = new Persona(nome);
+        Nodo nuovo = new Nodo(p);
+        nuovo.setLink(head);
+        if (head == null) {
+            tail = nuovo;
+        }
+        head = nuovo;
+        return "Persona aggiunta alla coda";
+    }
 }
--- cinema/Main.java
+++ cinema/Main.java
@@ -1,7 +1,48 @@
 package cinema;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        
+        int scelta = -1;
+        Coda b = new Coda();
+        do {
+            try {
+                scelta = -1;
+                Scanner t = new Scanner(System.in);
+                System.out.println("Menu:");
+                System.out.println("0. Esci");
+                System.out.println("1. Aggiungi persona");
+                System.out.println("2. Rimuovi persona in testa");
+                System.out.println("3. Estrai prossimo");
+                System.out.println("4. Visualizza coda");
+
+                if (t.hasNext()) {
+                    scelta = t.nextInt();
+                }
+                if (scelta < 0 || scelta > 4) {
+                    throw new IllegalArgumentException();
+                }
+            } catch (InputMismatchException | IllegalArgumentException e) {
+                System.out.println("Errore: scelta non valida");
+            }
+            switch (scelta) {
+                case 0:
+                    System.out.println("Uscita in corso");
+                    break;
+                case 1:
+                    System.out.println(b.addPersona());
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                default:
+                    break;
+            }
+        } while (scelta != 0);
     }
 }
