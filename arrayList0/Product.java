5a9e1465abb819a6212f3959ec1009844028e3fd
th3-7rue <66943187+th3-7rue@users.noreply.github.com>
2023-03-28 09:50:22 +0000

    d

---
+++ arrayList0/Product.java
@@ -0,0 +1,20 @@
+package arrayList0;
+
+public class Product {
+    private String nome;
+    private float prezzo;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public float getPrezzo() {
+        return prezzo;
+    }
+
+    public Product(String nome, float prezzo) {
+        this.nome = nome;
+        this.prezzo = prezzo;
+    }
+
+}
---
+++ arrayList0/ProductList.java
@@ -0,0 +1,11 @@
+package arrayList0;
+
+import java.util.ArrayList;
+
+public class ProductList {
+    private ArrayList<Product> l;
+
+    public ProductList() {
+        l = new ArrayList<Product>();
+    }
+}
