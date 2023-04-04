2c3b9f911266dc535b08c0e0cb0838e9bae23a6f
th3-7rue <66943187+th3-7rue@users.noreply.github.com>
2023-04-04 11:21:27 +0000

    d

--- arrayList1/ListaStudenti.java
+++ arrayList1/ListaStudenti.java
@@ -1,5 +1,33 @@
 package arrayList1;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 public class ListaStudenti {
-    
+    private ArrayList<Studente> l;
+
+    public ListaStudenti() {
+        l = new ArrayList<Studente>();
+    }
+
+    public void agginugiStudente(Studente studente) {
+        l.add(studente);
+    }
+
+    public void assegnaVoti() {
+        Random genera = new Random();
+        int votoMax = 10;
+        int votoMin = 1;
+        for (Studente studente : l) {
+            studente.setVoto1(genera.nextInt(votoMax - votoMin) + votoMin);
+            studente.setVoto2(genera.nextInt(votoMax - votoMin) + votoMin);
+            studente.setVoto3(genera.nextInt(votoMax - votoMin) + votoMin);
+        }
+    }
+
+    public void stampa() {
+        for (Studente studente : l) {
+            System.out.println(studente);
+        }
+    }
 }
--- arrayList1/Main.java
+++ arrayList1/Main.java
@@ -2,6 +2,14 @@ package arrayList1;
 
 public class Main {
     public static void main(String[] args) {
+        ListaStudenti l = new ListaStudenti();
+        Studente s1 = new Studente("Mario", "Rossi");
+        Studente s2 = new Studente("Luca", "Bianchi");
+        Studente s3 = new Studente("Giovanni", "Verdi");
+        Studente s4 = new Studente("Maria", "Neri");
+        Studente s5 = new Studente("Lucia", "Gialli");
 
+        l.assegnaVoti();
+        l.stampa();
     }
 }
