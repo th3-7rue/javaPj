e6451174b8434bb313ed16f2d77ab13892720a30
th3-7rue <66943187+th3-7rue@users.noreply.github.com>
2023-04-18 09:49:54 +0000

    t

--- comparable/Main.java
+++ comparable/Main.java
@@ -1,7 +1,10 @@
 package comparable;
 
+import java.util.ArrayList;
+
 public class Main {
+    private static final int NUMERO_PERSONE = 5;
     public static void main(String[] args) {
-        
+        ArrayList<Persona> persone = new ArrayList<>();
     }
 }
