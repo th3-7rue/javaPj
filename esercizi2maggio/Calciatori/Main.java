606173941e83524c3dd4452bdc1cc5ca4a852a97
th3-7rue <66943187+th3-7rue@users.noreply.github.com>
2023-05-02 11:03:26 +0000

    g

--- esercizi2maggio/Calciatori/Main.java
+++ esercizi2maggio/Calciatori/Main.java
@@ -1,5 +1,7 @@
 package esercizi2maggio.Calciatori;
 
 public class Main {
-    
+    public static void main(String[] args) {
+        
+    }
 }
--- esercizi2maggio/StudioNotarile/Main.java
+++ esercizi2maggio/StudioNotarile/Main.java
@@ -1,5 +1,7 @@
 package esercizi2maggio.StudioNotarile;
 
 public class Main {
-    
+    public static void main(String[] args) {
+        
+    }
 }
