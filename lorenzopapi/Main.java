d94e5142dea2111942cfd119a1fd647a8229a4ba
th3-7rue <66943187+th3-7rue@users.noreply.github.com>
2023-05-20 09:48:44 +0000

    y

--- lorenzopapi/Main.java
+++ lorenzopapi/Main.java
@@ -1,9 +1,47 @@
 package lorenzopapi;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         Registro r = new Registro();
-        r.carica();
-        r.salva();
+        // menu
+        String scelta = null;
+        Scanner sc = new Scanner(System.in);
+        do { 
+            System.out.println("Menu:");
+            System.out.println("1. Carica");
+            System.out.println("2. Salva");
+            System.out.println("3. Aggiungi prova");
+            System.out.println("4. Rimuovi prova");
+            System.out.println("5. Ordina");
+            System.out.println("6. Esci");
+            scelta = sc.next();
+
+            switch (scelta) {
+                case "1":
+                    r.carica();
+                    break;
+                case "2":
+                    r.salva();
+                    break;
+                case "3":
+                    r.aggiungiProva();
+                    break;
+                case "4":
+                    r.rimuoviProva();
+                    break;
+                case "5":
+                    r.ordina();
+                    break;
+                case "6":
+                    System.out.println("Arrivederci");
+                    break;
+                default:
+                    System.out.println("Scelta non valida");
+                    break;
+            }
+        } while (!scelta.equals("6"));
+        sc.close();
     }
 }
--- lorenzopapi/Registro.java
+++ lorenzopapi/Registro.java
@@ -22,7 +22,7 @@ public class Registro {
           BufferedReader br = new BufferedReader(new FileReader(inputPath));
           String line;
           while ((line = br.readLine()) != null) {
-              String[] campi = line.split(" ");
+              String[] campi = line.split(";");
             // crea oggetto Prova
             Prova p = new Prova(campi[0], campi[1], campi[2], campi[3], campi[4]);
             // aggiungi oggetto Prova in prove
@@ -41,7 +41,8 @@ public class Registro {
         try {
             fw = new FileWriter(outputPath);
             for (Prova p : prove) {
-                fw.write(p.getCognome() + " " + p.getNome() + " " + p.getMateria() + " " + p.getVoto() + " " + p.getData() + "\n");
+                fw.write(p.getCognome()
+                        + ";" + p.getNome() + ";" + p.getMateria() + ";" + p.getVoto() + ";" + p.getData() + "\n");
             }
             fw.close();
         } catch (Exception e) {
@@ -49,9 +50,73 @@ public class Registro {
         }
     }
     // aggiungi prova
+    public void aggiungiProva() {
+        // chiedi dati all'utente
+        // crea oggetto Prova
+        // aggiungi oggetto Prova in prove
+        System.out.println("aggiungi prova");
+        String cognome, nome, materia, voto, data;
+        System.out.println("inserisci cognome");
+        cognome = System.console().readLine();
+        System.out.println("inserisci nome");
+        nome = System.console().readLine();
+        System.out.println("inserisci materia");
+        materia = System.console().readLine();
+        System.out.println("inserisci voto");
+        voto = System.console().readLine();
+        System.out.println("inserisci data");
+        data = System.console().readLine();
+        Prova p = new Prova(cognome, nome, materia, voto, data);
+        prove.add(p);
 
+    }
     // rimuovi prova
-
+    public void rimuoviProva() {
+        // chiedi dati all'utente
+        // rimuovi oggetto Prova da prove
+        System.out.println("rimuovi prova");
+        System.out.println("inserisci cognome");
+        String cognome = System.console().readLine();
+        // stampa prove con quel cognome
+        int i = 1;
+        for (Prova p : prove) {
+            if (p.getCognome().equals(cognome)) {
+                System.out.println(
+                        i + ". " + p.getCognome() + " " + p.getNome() + " " + p.getMateria() + " " + p.getVoto() + " "
+                                + p.getData());
+                i++;
+            }
+        }
+        if(i==1) {
+            System.out.println("nessuna prova con questo cognome");
+            return;
+        }
+        // chiedi quale vuole rimuovere
+        System.out.println("quale vuoi rimuovere?");
+        int scelta;
+        do {
+            try {
+                scelta = Integer.parseInt(System.console().readLine());
+            } catch (Exception e) {
+                scelta = 0;
+            }
+            if (scelta < 1 || scelta > i) {
+                System.out.println("scelta non valida");
+            }
+        } while (scelta < 1 || scelta > i);
+        // rimuovi
+        for (Prova prova : prove) {
+            if (prova.getCognome().equals(cognome)) {
+                scelta--;
+                if (scelta == 0) {
+                    prove.remove(prova);
+                    break;
+                }
+            }
+        }
+    }
     // ordina
-
+    public void ordina() {
+        // ordina prove
+    }
 }
