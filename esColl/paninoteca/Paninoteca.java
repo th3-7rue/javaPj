5047c59b2e08c12159a3cbd62617b4211583df11
th3-7rue <66943187+th3-7rue@users.noreply.github.com>
2023-05-23 11:32:59 +0000

    f

--- esColl/paninoteca/Paninoteca.java
+++ esColl/paninoteca/Paninoteca.java
@@ -15,13 +15,13 @@ public class Paninoteca {
         String tipoPane = System.console().readLine();
         Panino panino = new Panino(tipoPane);
         // menu aggiungi/rimuovi ingrediente
-String scelta=null;
-        do{
-       System.out.println("1) Aggiungi ingrediente");
-         System.out.println("2) Rimuovi ingrediente");
+        String scelta = null;
+        do {
+            System.out.println("1) Aggiungi ingrediente");
+            System.out.println("2) Rimuovi ingrediente");
             System.out.println("0) Esci");
             scelta = System.console().readLine();
-            switch(scelta){
+            switch (scelta) {
                 case "1":
                     System.out.println("Ingrediente: ");
                     String ingrediente = System.console().readLine();
@@ -36,10 +36,25 @@ String scelta=null;
                     break;
                 default:
                     System.out.println("Scelta non valida");
-            }}
-    while(!scelta.equals("0");
+            }
+        } while (!scelta.equals("0"));
 
-    panini.add(panino);System.out.println("Panino aggiunto");
-}
+        panini.add(panino);
+        System.out.println("Panino aggiunto");
+    }
+
+    public void stampaPanini() {
+        System.out.println("Stampa panini");
+        for (Panino panino : panini) {
+            System.out.println(panino);
+        }
+    }
+    public void ordinaPerTipoPane() {
+        // ordina per tipo pane
+        System.out.println("Ordina per tipo pane");
+        panini.sort((p1, p2) -> p1.getTipoPane().compareTo(p2.getTipoPane()));
+        // stampa panini
+        stampaPanini();
+    }
 
 }
